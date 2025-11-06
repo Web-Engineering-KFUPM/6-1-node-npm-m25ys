219288b33867c247aa7674b4a928b7f709deff7c
m25ys <myalsuhaibani@gmail.com>
2025-11-06 09:48:03 +0300

    solved

--- utils/operations.js
+++ utils/operations.js
@@ -1,16 +1,39 @@
 export function add(numbers) {
-  return {/*write code here*/}
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    return 0;
+  }
+
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
 export function subtract(numbers) {
-  return {/*write code here*/}
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    return 0;
+  }
+
+  const [first, ...rest] = numbers;
+  return rest.reduce((total, num) => total - num, first);
 }
 
 export function multiply(numbers) {
-  return {/*write code here*/}
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    return 0;
+  }
+
+  const [first, ...rest] = numbers;
+  return rest.reduce((product, num) => product * num, first);
 }
 
 export function divide(numbers) {
-  return {/*write code here*/}
-}
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    return NaN;
+  }
 
+  const [first, ...rest] = numbers;
+
+  if (rest.some((num) => num === 0)) {
+    return NaN;
+  }
+
+  return rest.reduce((quotient, num) => quotient / num, first);
+}
