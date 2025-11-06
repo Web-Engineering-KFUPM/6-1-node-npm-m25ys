16344459c938fafc82dfa90de15727f330c6e0c1
m25ys <myalsuhaibani@gmail.com>
2025-11-06 09:46:53 +0300

    solve parser

--- utils/parser.js
+++ utils/parser.js
@@ -1,10 +1,10 @@
 import _ from "lodash";
 
 export function parseNumbers(input) {
-  return {/*write code here*/}
+  return _.filter(_.map(input, (value) => Number(value)), (num) => !Number.isNaN(num))
 }
 
 export function isValidOperation(operation) {
-  return {/*write code here*/}
+  return _.includes(["add", "subtract", "multiply", "divide"], operation);
 }
 
